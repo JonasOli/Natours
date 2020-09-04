33d16e6395ebf4e513c9fbef40d26fd0604ba4f0
Jonas Oliveira <jonasfissicaro13@gmail.com>
2020-09-03 21:58:48 -0300

    tours section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { About } from "../../components/About";
 import { Feature } from "../../components/Features";
 import { Header } from "../../components/Header";
+import { Tours } from "../../components/Tours";
 
 export const Home = () => {
   return (
@@ -9,6 +10,7 @@ export const Home = () => {
       <Header />
       <About />
       <Feature />
+      <Tours />
     </>
   );
 };
