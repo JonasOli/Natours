eed0929a7251106d135bdb9ddb1f266427c57586
Jonas Oliveira <jonasfissicaro13@gmail.com>
2021-03-20 20:11:16 -0300

    navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ import { Booking } from "../../components/Booking";
 import { Feature } from "../../components/Features";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { Navigation } from "../../components/Navigation";
 import { Story } from "../../components/Story";
 import { Tours } from "../../components/Tours";
 import "./style.scss";
@@ -11,6 +12,7 @@ import "./style.scss";
 export const Home = () => {
   return (
     <>
+      <Navigation />
       <Header />
       <About />
       <Feature />
